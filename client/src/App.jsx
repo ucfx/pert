2c52032d10cca899c9756032d6a9b4667fb41cb5
youcefhmd <65844371+youcefhmd@users.noreply.github.com>
2023-12-02 07:46:41 +0100

    Add zoom in/out and drag functionality to the diagram

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Login, Register } from "pages";
 import { PertChart } from "./components";
+import { useState } from "react";
 
 function App() {
-    const data = [
+    const [data, setData] = useState([
         { key: 0, length: 0, text: "Start" },
         { key: 1, length: 6, text: "A", dependsOn: [5] },
+        { key: 2, length: 5, text: "B", dependsOn: [10, 5] },
+        { key: 3, length: 1, text: "C", dependsOn: [0] },
+        { key: 4, length: 1, text: "D", dependsOn: [0] },
+        { key: 5, length: 2, text: "E", dependsOn: [0] },
+        { key: 6, length: 6, text: "F", dependsOn: [4] },
+        { key: 7, length: 3, text: "G", dependsOn: [6, 4] },
+        { key: 8, length: 4, text: "H", dependsOn: [1, 3, 4] },
+        {
+            key: 9,
+            length: 1,
+            text: "I",
+            dependsOn: [8, 1, 3, 4, 5, 11, 6],
+        },
+        { key: 10, length: 4, text: "J", dependsOn: [5] },
+        { key: 11, length: 8, text: "K", dependsOn: [4, 6] },
+    ]);
+
+    let newData = [
+        { key: 0, length: 0, text: "Start" },
+        { key: 1, length: 10, text: "A", dependsOn: [5] },
         { key: 2, length: 5, text: "B", dependsOn: [10, 5] },
         { key: 3, length: 1, text: "C", dependsOn: [0] },
         { key: 4, length: 1, text: "D", dependsOn: [0] },
@@ -29,9 +50,24 @@ function App() {
                 <Route
                     path="/pert"
                     element={
-                        <div className="pert-chart">
-                            <PertChart data={data} />
-                        </div>
+                        <>
+                            <button onClick={() => setData(newData)}>
+                                Click me
+                            </button>
+                            <div
+                                className="pert-chart"
+                                style={{
+                                    width: 800,
+                                    height: 400,
+                                }}
+                            >
+                                <PertChart
+                                    data={data}
+                                    containerWidth={800}
+                                    containerHeight={400}
+                                />
+                            </div>
+                        </>
                     }
                 />
                 <Route path="/login" element={<Login />} />
--- client/src/components/PertChart/index.jsx
+++ client/src/components/PertChart/index.jsx
@@ -1,22 +1,122 @@
-import { useRef, useEffect, useMemo } from "react";
+import { useRef, useEffect, useMemo, useState } from "react";
 import Pert from "assets/js/Pert.js";
 import Drawer from "assets/js/Drawer.js";
+import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
 import "./style.css";
-const PertChart = ({ data }) => {
-    const ref = useRef();
+
+const PertChart = ({ data, containerWidth, containerHeight }) => {
+    const svgElement = useRef();
+    const [imageNaturalWidth, setImageNaturalWidth] = useState(0);
+    const [imageNaturalHeight, setImageNaturalHeight] = useState(0);
+
+    const [nodes, levels, links] = useMemo(() => {
+        const pert = new Pert(data.map((item) => ({ ...item }))).solve();
+        console.log("Done");
+        return [pert.nodes, pert.levels, pert.links];
+    }, [JSON.stringify(data)]);
+
+    const scaleUp = true;
+    const zoomFactor = 8;
 
     useEffect(() => {
-        const pert = new Pert(data).solve();
-        new Drawer(
-            ref.current,
-            pert.nodes,
-            pert.levels,
-            pert.links
-        ).drawPERTDiagram();
-    }, []);
+        if (!svgElement.current) return;
+
+        const maxHeight =
+            Math.max(...Object.keys(levels).map((key) => levels[key].length)) *
+                140 -
+            35;
+
+        const maxWidth = (Object.values(levels).length - 1) * 140 + 90;
+
+        setImageNaturalHeight(maxHeight);
+        setImageNaturalWidth(maxWidth);
+
+        svgElement.current.setAttribute("height", maxHeight);
+        svgElement.current.setAttribute("width", maxWidth);
+    }, [nodes, levels, links, svgElement.current]);
+
+    useEffect(() => {
+        if (
+            nodes.length === 0 ||
+            containerWidth === 0 ||
+            containerHeight === 0 ||
+            imageNaturalHeight === 0 ||
+            imageNaturalWidth === 0
+        )
+            return;
+
+        new Drawer(svgElement.current, nodes, levels, links).drawPERTDiagram();
+    }, [
+        containerWidth,
+        containerHeight,
+        imageNaturalWidth,
+        imageNaturalHeight,
+        data,
+    ]);
+
+    const imageScale = useMemo(() => {
+        if (
+            containerWidth === 0 ||
+            containerHeight === 0 ||
+            imageNaturalWidth === 0 ||
+            imageNaturalHeight === 0
+        )
+            return 1;
+
+        const scale = Math.min(
+            containerWidth / imageNaturalWidth,
+            containerHeight / imageNaturalHeight
+        );
+        return scaleUp ? scale : Math.max(scale, 1);
+    }, [
+        scaleUp,
+        containerWidth,
+        containerHeight,
+        imageNaturalWidth,
+        imageNaturalHeight,
+    ]);
+
     return (
         <div className="chart">
-            <svg ref={ref} />
+            <TransformWrapper
+                key={`${containerWidth}x${containerHeight}`}
+                initialScale={imageScale}
+                minScale={imageScale}
+                maxScale={imageScale * zoomFactor}
+                centerOnInit
+            >
+                {({ zoomIn, zoomOut, resetTransform, centerView }) => (
+                    <>
+                        <div className="chart-controls">
+                            <button onClick={() => zoomIn()}>
+                                <i className="fa-regular fa-circle-plus" />
+                            </button>
+                            <button onClick={() => zoomOut()}>
+                                <i className="fa-regular fa-circle-minus" />
+                            </button>
+                            <button onClick={() => centerView(imageScale)}>
+                                <i className="fa-regular fa-compress" />
+                            </button>
+                        </div>
+                        <TransformComponent
+                            wrapperStyle={{
+                                width: "100%",
+                                height: "100%",
+                            }}
+                        >
+                            <svg
+                                ref={svgElement}
+                                onLoadCapture={() => {
+                                    setTimeout(() => {
+                                        resetTransform();
+                                        centerView(imageScale);
+                                    }, 400);
+                                }}
+                            />
+                        </TransformComponent>
+                    </>
+                )}
+            </TransformWrapper>
         </div>
     );
 };
