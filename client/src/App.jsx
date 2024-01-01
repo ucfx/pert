f2282dcc8644cb27889bd8d0c16e9b4f8dffe74b
Nadhir <nadhir.hb@protonmail.com>
2024-01-01 21:39:16 +0100

    feat: add table to manipulate chart

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Navigate,
-    useParams,
-    useLocation,
-    Link,
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+	useParams,
+	useLocation,
+	Link,
 } from "react-router-dom";
 import { lazy, Suspense } from "react";
 const Loader = lazy(() => import("components/Loader"));
@@ -18,205 +18,144 @@ const PertChart = lazy(() => import("components/PertChart"));
 const ProjectDetails = lazy(() => import("pages/Projects/ProjectDetails"));
 const NotFound = lazy(() => import("components/NotFound"));
 import useAuth from "hooks/useAuth";
+import Table from "./components/Table";
+
+import "@mantine/core/styles.css";
+
+import { MantineProvider } from "@mantine/core";
+
 const PageTitleProvider = lazy(() =>
-    import("context/PageTitleContext").then((module) => {
-        return { default: module.PageTitleProvider };
-    })
+	import("context/PageTitleContext").then((module) => {
+		return { default: module.PageTitleProvider };
+	})
 );
 const InfoProvider = lazy(() =>
-    import("context/InfoContext").then((module) => {
-        return { default: module.InfoProvider };
-    })
+	import("context/InfoContext").then((module) => {
+		return { default: module.InfoProvider };
+	})
 );
 const ProjectProvider = lazy(() =>
-    import("context/ProjectContext").then((module) => {
-        return { default: module.ProjectProvider };
-    })
+	import("context/ProjectContext").then((module) => {
+		return { default: module.ProjectProvider };
+	})
 );
 const ProjectDetailsProvider = lazy(() =>
-    import("context/ProjectDetailsContext").then((module) => {
-        return { default: module.ProjectDetailsProvider };
-    })
+	import("context/ProjectDetailsContext").then((module) => {
+		return { default: module.ProjectDetailsProvider };
+	})
 );
-import { useState } from "react";
 
 function App() {
-    const [data, setData] = useState([
-        // { key: 0, length: 0, text: "Start" },
-        { key: 1, length: 6, text: "A", dependsOn: [5] },
-        { key: 2, length: 5, text: "B", dependsOn: [10, 5] },
-        { key: 3, length: 1, text: "C" },
-        { key: 4, length: 1, text: "D" },
-        { key: 5, length: 2, text: "E" },
-        { key: 6, length: 6, text: "F", dependsOn: [4] },
-        { key: 7, length: 3, text: "G", dependsOn: [6, 4] },
-        { key: 8, length: 4, text: "H", dependsOn: [1, 3, 4] },
-        {
-            key: 9,
-            length: 1,
-            text: "I",
-            dependsOn: [8, 1, 3, 4, 5, 11, 6],
-        },
-        { key: 10, length: 4, text: "J", dependsOn: [5] },
-        { key: 11, length: 8, text: "K", dependsOn: [4, 6] },
-    ]);
-
-    let newData = [
-        // { key: 0, length: 0, text: "Start" },
-        { key: 1, length: 10, text: "A", dependsOn: [5] },
-        { key: 2, length: 5, text: "B", dependsOn: [10, 5] },
-        { key: 3, length: 1, text: "C" },
-        { key: 4, length: 1, text: "D" },
-        { key: 5, length: 2, text: "E" },
-        { key: 6, length: 6, text: "F", dependsOn: [4] },
-        { key: 7, length: 3, text: "G", dependsOn: [6, 4] },
-        { key: 8, length: 4, text: "H", dependsOn: [1, 3, 4] },
-        {
-            key: 9,
-            length: 1,
-            text: "I",
-            dependsOn: [8, 1, 3, 4, 5, 11, 6],
-        },
-        { key: 10, length: 4, text: "J", dependsOn: [5] },
-        { key: 11, length: 8, text: "K", dependsOn: [4, 6] },
-    ];
-    const { user } = useAuth();
-    return (
-        // <Suspense fallback={<Loader />}>
-        <Router>
-            <Routes>
-                <Route
-                    path="/:username"
-                    element={
-                        <Suspense fallback={<Loader />}>
-                            <AuthRoute>
-                                <PageTitleProvider>
-                                    <InfoProvider>
-                                        <UserProfile />
-                                    </InfoProvider>
-                                </PageTitleProvider>
-                            </AuthRoute>
-                        </Suspense>
-                    }
-                >
-                    <Route
-                        index
-                        element={<Navigate to="projects" replace={true} />}
-                    />
-                    <Route
-                        path="projects"
-                        element={
-                            <AuthRoute>
-                                <ProjectProvider>
-                                    <Suspense fallback={<Loader />}>
-                                        <Projects />
-                                    </Suspense>
-                                </ProjectProvider>
-                            </AuthRoute>
-                        }
-                    />
-                    <Route
-                        path="projects/:projectId"
-                        element={
-                            <Suspense fallback={<Loader />}>
-                                <ProjectDetailsProvider>
-                                    <ProjectDetails />
-                                </ProjectDetailsProvider>
-                            </Suspense>
-                        }
-                    />
-                    <Route
-                        path={"settings"}
-                        element={
-                            <AuthRoute>
-                                <Suspense fallback={<Loader />}>
-                                    <Settings />
-                                </Suspense>
-                            </AuthRoute>
-                        }
-                        index
-                    />
-                </Route>
-                <Route
-                    path="/login"
-                    element={
-                        user ? (
-                            <Navigate to="/dashboard" replace={true} />
-                        ) : (
-                            <Suspense fallback={<Loader />}>
-                                <Login />
-                            </Suspense>
-                        )
-                    }
-                />
-                <Route
-                    path="/register"
-                    element={
-                        user ? (
-                            <Navigate to="/dashboard" replace={true} />
-                        ) : (
-                            <Suspense fallback={<Loader />}>
-                                <Register />
-                            </Suspense>
-                        )
-                    }
-                />
-                <Route
-                    path="/pert"
-                    element={
-                        <>
-                            <button onClick={() => setData(newData)}>
-                                Click me
-                            </button>
-                            <div
-                                className="pert-chart"
-                                style={{
-                                    width: 800,
-                                    height: 400,
-                                }}
-                            >
-                                <PertChart
-                                    data={data}
-                                    containerWidth={800}
-                                    containerHeight={400}
-                                />
-                            </div>
-                        </>
-                    }
-                />
-                <Route
-                    path="/"
-                    element={
-                        <>
-                            home <Link to="/login">login</Link>
-                        </>
-                    }
-                />
-                // 404
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </Router>
-        // </Suspense>
-    );
+	const { user } = useAuth();
+	return (
+		// <Suspense fallback={<Loader />}>
+		<MantineProvider forceColorScheme="dark">
+			<Router>
+				<Routes>
+					<Route
+						path="/:username"
+						element={
+							<Suspense fallback={<Loader />}>
+								<AuthRoute>
+									<PageTitleProvider>
+										<InfoProvider>
+											<UserProfile />
+										</InfoProvider>
+									</PageTitleProvider>
+								</AuthRoute>
+							</Suspense>
+						}
+					>
+						<Route index element={<Navigate to="projects" replace={true} />} />
+						<Route
+							path="projects"
+							element={
+								<AuthRoute>
+									<ProjectProvider>
+										<Suspense fallback={<Loader />}>
+											<Projects />
+										</Suspense>
+									</ProjectProvider>
+								</AuthRoute>
+							}
+						/>
+						<Route
+							path="projects/:projectId"
+							element={
+								<Suspense fallback={<Loader />}>
+									<ProjectDetailsProvider>
+										<ProjectDetails />
+									</ProjectDetailsProvider>
+								</Suspense>
+							}
+						/>
+						<Route
+							path={"settings"}
+							element={
+								<AuthRoute>
+									<Suspense fallback={<Loader />}>
+										<Settings />
+									</Suspense>
+								</AuthRoute>
+							}
+							index
+						/>
+					</Route>
+					<Route
+						path="/login"
+						element={
+							user ? (
+								<Navigate to="/dashboard" replace={true} />
+							) : (
+								<Suspense fallback={<Loader />}>
+									<Login />
+								</Suspense>
+							)
+						}
+					/>
+					<Route
+						path="/register"
+						element={
+							user ? (
+								<Navigate to="/dashboard" replace={true} />
+							) : (
+								<Suspense fallback={<Loader />}>
+									<Register />
+								</Suspense>
+							)
+						}
+					/>
+					<Route path="/pert" element={<Table />} />
+					<Route
+						path="/"
+						element={
+							<>
+								home <Link to="/login">login</Link>
+							</>
+						}
+					/>
+					404
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</Router>
+		</MantineProvider>
+	);
 }
 
 export default App;
 
 const AuthRoute = ({ children }) => {
-    const { user } = useAuth();
-    const { username } = useParams();
-    const { pathname } = useLocation();
+	const { user } = useAuth();
+	const { username } = useParams();
+	const { pathname } = useLocation();
 
-    if (!user) {
-        return <Navigate to="/login" replace={true} />;
-    } else if (user.username !== username) {
-        return (
-            <Navigate
-                to={pathname.replace(username, user.username)}
-                replace={true}
-            />
-        );
-    }
+	if (!user) {
+		return <Navigate to="/login" replace={true} />;
+	} else if (user.username !== username) {
+		return (
+			<Navigate to={pathname.replace(username, user.username)} replace={true} />
+		);
+	}
 
-    return children;
+	return children;
 };
--- client/src/assets/js/Pert.js
+++ client/src/assets/js/Pert.js
@@ -1,182 +1,173 @@
 "use strict";
 
 class Pert {
-    constructor(data) {
-        this.data = [
-            { key: 0, length: 0, text: "Start" },
-            ...data.map((_) =>
-                _.dependsOn ? { ..._ } : { ..._, dependsOn: [0] }
-            ),
-        ];
-        console.log(this.data);
-        this.nodes = null;
-        this.levels = null;
-    }
+	constructor(data) {
+		this.data = [
+			{ key: "0", length: 0, text: "Start" },
+			...data.map((_) => (_.dependsOn ? { ..._ } : { ..._, dependsOn: ["0"] })),
+		];
+		console.log(this.data);
+		this.nodes = null;
+		this.levels = null;
+	}
 
-    calculatePERT() {
-        this.nodes = null;
-        this.levels = null;
+	calculatePERT() {
+		this.nodes = null;
+		this.levels = null;
 
-        this.nodes = [...this.data];
+		this.nodes = [...this.data];
 
-        if (this.levels === null) this.getLevels();
+		if (this.levels === null) this.getLevels();
 
-        let levelKeys = Object.keys(this.levels);
-        levelKeys.forEach((levelKey) => {
-            this.levels[levelKey].forEach((index) => {
-                this.calculateEarlyTimes(index);
-            });
-        });
+		let levelKeys = Object.keys(this.levels);
+		levelKeys.forEach((levelKey) => {
+			this.levels[levelKey].forEach((index) => {
+				this.calculateEarlyTimes(index);
+			});
+		});
 
-        let lastTask = this.nodes.reduce((prev, current) =>
-            prev.earlyFinish > current.earlyFinish ? prev : current
-        );
-        lastTask.lateFinish = lastTask.earlyFinish;
-        lastTask.critical = true;
-        levelKeys = levelKeys.sort((a, b) => b - a);
-        this.nodes.push({
-            key: this.nodes.length,
-            text: "Finish",
-            dependsOn: [lastTask.key],
-            length: 0,
-            earlyStart: lastTask.earlyFinish,
-            earlyFinish: lastTask.earlyFinish,
-            lateFinish: lastTask.earlyFinish,
-            lateStart: lastTask.earlyFinish,
-            level: lastTask.level + 1,
-            critical: true,
-        });
+		let lastTask = this.nodes.reduce((prev, current) =>
+			prev.earlyFinish > current.earlyFinish ? prev : current
+		);
+		lastTask.lateFinish = lastTask.earlyFinish;
+		lastTask.critical = true;
+		levelKeys = levelKeys.sort((a, b) => b - a);
+		this.nodes.push({
+			key: this.nodes.length.toString(),
+			text: "Finish",
+			dependsOn: [lastTask.key],
+			length: 0,
+			earlyStart: lastTask.earlyFinish,
+			earlyFinish: lastTask.earlyFinish,
+			lateFinish: lastTask.earlyFinish,
+			lateStart: lastTask.earlyFinish,
+			level: lastTask.level + 1,
+			critical: true,
+		});
 
-        this.levels[lastTask.level + 1] = [this.nodes.length - 1];
+		this.levels[lastTask.level + 1] = [this.nodes.length - 1];
 
-        levelKeys.forEach((levelKey) => {
-            this.levels[levelKey].forEach((index) => {
-                this.calculateLateTimes(index, lastTask.lateFinish);
-            });
-        });
-        return this.nodes;
-    }
+		levelKeys.forEach((levelKey) => {
+			this.levels[levelKey].forEach((index) => {
+				this.calculateLateTimes(index, lastTask.lateFinish);
+			});
+		});
+		return this.nodes;
+	}
 
-    getSuccessors(task) {
-        return this.nodes.filter(
-            (t) => t.dependsOn && t.dependsOn.includes(task.key)
-        );
-    }
+	getSuccessors(task) {
+		return this.nodes.filter(
+			(t) => t.dependsOn && t.dependsOn.includes(task.key)
+		);
+	}
 
-    calculateEarlyTimes(index) {
-        let task = this.nodes[index];
-        if (!task.dependsOn) {
-            task.earlyStart = 0;
-        } else {
-            let maxFinishTime = 0;
-            task.dependsOn.forEach((dependency) => {
-                let dependencyTask = this.nodes.find(
-                    (item) => item.key === dependency
-                );
-                maxFinishTime = Math.max(
-                    maxFinishTime,
-                    dependencyTask.earlyStart + dependencyTask.length
-                );
-            });
-            task.earlyStart = maxFinishTime;
-        }
-        task.earlyFinish = task.earlyStart + task.length;
-    }
+	calculateEarlyTimes(index) {
+		let task = this.nodes[index];
+		if (!task.dependsOn) {
+			task.earlyStart = 0;
+		} else {
+			let maxFinishTime = 0;
+			task.dependsOn.forEach((dependency) => {
+				let dependencyTask = this.nodes.find((item) => item.key === dependency);
+				maxFinishTime = Math.max(
+					maxFinishTime,
+					dependencyTask.earlyStart + dependencyTask.length
+				);
+			});
+			task.earlyStart = maxFinishTime;
+		}
+		task.earlyFinish = task.earlyStart + task.length;
+	}
 
-    calculateLateTimes(index, lateFinishLastTask) {
-        let task = this.nodes[index];
-        let successors = this.getSuccessors(task);
+	calculateLateTimes(index, lateFinishLastTask) {
+		let task = this.nodes[index];
+		let successors = this.getSuccessors(task);
 
-        let lateFinish;
-        if (successors.length === 0) {
-            lateFinish = lateFinishLastTask;
-        } else {
-            lateFinish = Math.min(
-                ...successors.map((s) => s.lateFinish - s.length)
-            );
-        }
+		let lateFinish;
+		if (successors.length === 0) {
+			lateFinish = lateFinishLastTask;
+		} else {
+			lateFinish = Math.min(...successors.map((s) => s.lateFinish - s.length));
+		}
 
-        task.lateFinish = lateFinish;
-        task.lateStart = task.lateFinish - task.length;
-        task.critical = task.earlyFinish === task.lateFinish;
-    }
+		task.lateFinish = lateFinish;
+		task.lateStart = task.lateFinish - task.length;
+		task.critical = task.earlyFinish === task.lateFinish;
+	}
 
-    getLevels() {
-        this.levels = {};
-        let nodes = [...this.data];
-        const calcLevel = (task) => {
-            if (!task.dependsOn) {
-                task.level = 0;
-            } else {
-                let maxLevel = 0;
-                task.dependsOn.forEach((dependency) => {
-                    let dependencyTask = nodes.find(
-                        (item) => item.key === dependency
-                    );
-                    if (dependencyTask.level === undefined) {
-                        calcLevel(dependencyTask);
-                    }
-                    maxLevel = Math.max(maxLevel, dependencyTask.level + 1);
-                });
-                task.level = maxLevel;
-            }
-            if (!this.levels[task.level]) {
-                this.levels[task.level] = [];
-            }
-            if (!this.levels[task.level].includes(task.key)) {
-                this.levels[task.level].push(task.key);
-            }
-        };
+	getLevels() {
+		this.levels = {};
+		let nodes = [...this.data];
+		const calcLevel = (task) => {
+			if (!task.dependsOn) {
+				task.level = 0;
+			} else {
+				let maxLevel = 0;
+				task.dependsOn.forEach((dependency) => {
+					let dependencyTask = nodes.find((item) => item.key === dependency);
+					if (dependencyTask.level === undefined) {
+						calcLevel(dependencyTask);
+					}
+					maxLevel = Math.max(maxLevel, dependencyTask.level + 1);
+				});
+				task.level = maxLevel;
+			}
+			if (!this.levels[task.level]) {
+				this.levels[task.level] = [];
+			}
+			if (!this.levels[task.level].includes(task.key)) {
+				this.levels[task.level].push(task.key);
+			}
+		};
 
-        nodes.forEach((task) => {
-            calcLevel(task);
-        });
+		nodes.forEach((task) => {
+			calcLevel(task);
+		});
 
-        let levelKeys = Object.keys(this.levels);
-        levelKeys.forEach((levelKey) => {
-            this.levels[levelKey] = this.levels[levelKey].sort((a, b) => a - b);
-        });
-    }
+		let levelKeys = Object.keys(this.levels);
+		levelKeys.forEach((levelKey) => {
+			this.levels[levelKey] = this.levels[levelKey].sort((a, b) => a - b);
+		});
+	}
 
-    getNodeLinks() {
-        let linkData = [];
-        let dependencies = [];
-        this.nodes.forEach((task, index) => {
-            if (index === 0) return;
-            if (task.dependsOn) {
-                task.dependsOn.forEach((dependency) => {
-                    dependencies.push(dependency);
-                    linkData.push({ from: dependency, to: task.key });
-                });
-            } else {
-                linkData.push({ from: 0, to: task.key });
-            }
-        });
+	getNodeLinks() {
+		let linkData = [];
+		let dependencies = [];
+		this.nodes.forEach((task, index) => {
+			if (index === 0) return;
+			if (task.dependsOn) {
+				task.dependsOn.forEach((dependency) => {
+					dependencies.push(dependency);
+					linkData.push({ from: dependency, to: task.key });
+				});
+			} else {
+				linkData.push({ from: 0, to: task.key });
+			}
+		});
 
-        // Add links to finish node
-        this.nodes
-            .filter(
-                (task) =>
-                    !dependencies.includes(task.key) &&
-                    task.key !== this.nodes.length - 1
-            )
-            .forEach((node) => {
-                linkData.push({
-                    from: node.key,
-                    to: this.nodes[this.nodes.length - 1].key,
-                });
-            });
+		// Add links to finish node
+		this.nodes
+			.filter(
+				(task) =>
+					!dependencies.includes(task.key) && task.key !== this.nodes.length - 1
+			)
+			.forEach((node) => {
+				linkData.push({
+					from: node.key,
+					to: this.nodes[this.nodes.length - 1].key,
+				});
+			});
 
-        return linkData;
-    }
+		return linkData;
+	}
 
-    solve() {
-        return {
-            nodes: this.calculatePERT(),
-            levels: this.levels,
-            links: this.getNodeLinks(),
-        };
-    }
+	solve() {
+		return {
+			nodes: this.calculatePERT(),
+			levels: this.levels,
+			links: this.getNodeLinks(),
+		};
+	}
 }
 
 export default Pert;
